b1497df85df25c9296fd5f2a1a65d45bff07e01e
Anup Khanal <anup@uxcam.com>
2022-06-20 17:40:18 +0545

    fix issue with video player library, screen in demo app to test fix

--- UXCamReactExample/App.js
+++ UXCamReactExample/App.js
@@ -11,6 +11,9 @@ import {Platform, StyleSheet, Text, View, Button} from 'react-native';
 import RNUxcam from 'react-native-ux-cam';
 import {UXCamOcclusionType} from 'react-native-ux-cam/UXCamOcclusion';
 import {launchImageLibrary} from 'react-native-image-picker';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Video from 'react-native-video';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -23,22 +26,116 @@ const instructions = Platform.select({
 var hideScreen = true;
 
 type Props = {};
+
+function HomeScreen({navigation}) {
+  let options = {
+    title: 'Select Image',
+    customButtons: [
+      {name: 'customOptionKey', title: 'Choose Photo from Custom Option'},
+    ],
+    storageOptions: {
+      skipBackup: true,
+      path: 'images',
+    },
+  };
+
+  function _handlePress(event) {
+    // Toggle the whole screen being hidden
+    // RNUxcam.occludeSensitiveScreen(hideScreen, hideScreen);
+    hideScreen = !hideScreen;
+  }
+
+  function _handleEmptyPress(event) {
+    // Just a placeholder button handler
+    console.log('A button was pressed');
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Welcome to React Native!</Text>
+      <Text style={styles.instructions}>To get started, edit App.js</Text>
+      <Text style={styles.instructions}>{instructions}</Text>
+
+      <Button
+        onPress={() => {
+          RNUxcam.allowShortBreakForAnotherApp(true);
+          launchImageLibrary(options, (response) => {
+            RNUxcam.allowShortBreakForAnotherApp(false);
+          });
+        }}
+        title="Open Gallery"
+      />
+      <Button
+        onPress={() => {
+          console.log(ram);
+        }}
+        title="Crash"
+      />
+      <Button
+        onPress={() => {
+          navigation.navigate('Video Screen');
+        }}
+        title="Video Screen"
+      />
+      <Text
+        ref={(label) => {
+          RNUxcam.occludeSensitiveView(label);
+        }}
+        style={styles.label}>
+        {' '}
+        {'Label hidden from UXCam'}{' '}
+      </Text>
+      <Button
+        title="Press here to toggle hiding whole screen"
+        onPress={_handlePress()}
+      />
+      <Button
+        ref={(x) => {
+          RNUxcam.occludeSensitiveViewWithoutGesture(x);
+        }}
+        title="Gestures on this hidden button not recorded"
+        onPress={_handleEmptyPress()}
+      />
+    </View>
+  );
+}
+
+function VideoScreen() {
+  return (
+    <Video
+      source={{uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4'}}
+      controls={true}
+      style={{width: 400, height: 400}}
+      muted={false}
+      repeat={false}
+      resizeMode={'cover'}
+      volume={1.0}
+      rate={1.0}
+      ignoreSilentSwitch={'ignore'}
+      playWhenInactive={true}
+      playInBackground={true}
+    />
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
 export default class App extends Component<Props> {
   componentDidMount() {
     RNUxcam.optIntoSchematicRecordings();
     const blur = {
       type: UXCamOcclusionType.Blur,
       blurRadius: 10,
+      hideGestures: false,
     };
     const occludeTextFields = {
-      type: UXCamOcclusionType.OccludeAllTextFields
-    }
+      type: UXCamOcclusionType.OccludeAllTextFields,
+    };
     const configuration = {
       userAppKey: 'flfqcjxcg4q0f85',
-      occlusions: [blur, occludeTextFields],
       enableImprovedScreenCapture: true,
+      // occlusions: [blur, OccludeAllTextFields]
     };
-
     RNUxcam.startWithConfiguration(configuration);
     RNUxcam.addVerificationListener((result) =>
       console.log(`UXCam: verificationResult: ${JSON.stringify(result)}`),
@@ -46,71 +143,14 @@ export default class App extends Component<Props> {
   }
 
   render() {
-    let options = {
-      title: 'Select Image',
-      customButtons: [
-        {name: 'customOptionKey', title: 'Choose Photo from Custom Option'},
-      ],
-      storageOptions: {
-        skipBackup: true,
-        path: 'images',
-      },
-    };
-
-    // Basic instructions and then some examples of occluding views from the UXCam recording
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-
-        <Button
-          onPress={() => {
-            RNUxcam.allowShortBreakForAnotherApp(true);
-            launchImageLibrary(options, (response) => {
-              RNUxcam.allowShortBreakForAnotherApp(false);
-            });
-          }}
-          title="Open Gallery"
-        />
-        <Button
-          onPress={() => {
-            console.log(ram);
-          }}
-          title="Crash"
-        />
-        <Text
-          ref={(label) => {
-            RNUxcam.occludeSensitiveView(label);
-          }}
-          style={styles.label}>
-          {' '}
-          {'Label hidden from UXCam'}{' '}
-        </Text>
-        <Button
-          title="Press here to toggle hiding whole screen"
-          onPress={this._handlePress}
-        />
-        <Button
-          ref={(x) => {
-            RNUxcam.occludeSensitiveViewWithoutGesture(x);
-          }}
-          title="Gestures on this hidden button not recorded"
-          onPress={this._handleEmptyPress}
-        />
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Video Screen" component={VideoScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
-  }
-
-  _handlePress(event) {
-    // Toggle the whole screen being hidden
-    RNUxcam.occludeSensitiveScreen(hideScreen, hideScreen);
-    hideScreen = !hideScreen;
-  }
-
-  _handleEmptyPress(event) {
-    // Just a placeholder button handler
-    console.log('A button was pressed');
   }
 }
 
@@ -135,5 +175,13 @@ const styles = StyleSheet.create({
     fontSize: 18,
     textAlign: 'center',
     margin: 10,
+  },
+  video: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    flex: 1,
   },
 });
