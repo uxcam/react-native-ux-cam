a884b578e7559d972180e426c6083a8c5f472ba4
Bishwash Adhikari <bishwash@uxcam.com>
2024-01-25 11:06:15 +0545

    fix: added missing NoSuchKeyException and UnexpectedNativeTypeException imports

--- uxcam-react-wrapper/android/src/main/java/com/uxcam/RNUxcamModule.java
+++ uxcam-react-wrapper/android/src/main/java/com/uxcam/RNUxcamModule.java
@@ -3,6 +3,7 @@ package com.uxcam;
 import android.view.View;
 
 import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.NoSuchKeyException;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
@@ -12,6 +13,7 @@ import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.ReadableType;
 import com.facebook.react.bridge.ReadableMapKeySetIterator;
+import com.facebook.react.bridge.UnexpectedNativeTypeException;
 import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
@@ -306,7 +308,7 @@ public class RNUxcamModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void logEvent(String event) {
-      UXCam.logEvent(event);
+        UXCam.logEvent(event);
     }
 
     @ReactMethod
