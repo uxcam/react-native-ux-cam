e404e007990cc4b6abb2d19010d1c6367fedb77c
ankit-uxcam <ankit@uxcam.com>
2025-11-19 10:47:19 +0545

    Fix issues for running locally

--- UXCamReactExample-Typescript/metro.config.js
+++ UXCamReactExample-Typescript/metro.config.js
@@ -10,11 +10,16 @@ const path = require('path');
 const config = {
   watchFolders: [
     path.resolve(__dirname, '../uxcam-react-wrapper'),
+    path.resolve(__dirname, 'node_modules'),
   ],
   resolver: {
     alias: {
       'react-native-ux-cam': path.resolve(__dirname, '../uxcam-react-wrapper'),
     },
+    nodeModulesPaths: [
+      path.resolve(__dirname, 'node_modules'),
+      path.resolve(__dirname, '../uxcam-react-wrapper/node_modules'),
+    ],
   },
 };
 
