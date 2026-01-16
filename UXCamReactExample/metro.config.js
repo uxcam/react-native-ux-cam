555e2d3165bd235a8aa75ca688a12673461d1223
ankit-uxcam <ankit@uxcam.com>
2026-01-16 16:13:40 +0545

    Allow demo apps to find uxcam wrapper

--- UXCamReactExample/metro.config.js
+++ UXCamReactExample/metro.config.js
@@ -1,11 +1,28 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require('path');
 
 /**
  * Metro configuration
- * https://reactnative.dev/docs/metro
+ * https://facebook.github.io/metro/docs/configuration
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  watchFolders: [
+    path.resolve(__dirname, '../uxcam-react-wrapper'),
+  ],
+  resolver: {
+    alias: {
+      'react-native-ux-cam': path.resolve(__dirname, '../uxcam-react-wrapper'),
+    },
+    nodeModulesPaths: [
+      path.resolve(__dirname, '../uxcam-react-wrapper/node_modules'),
+    ],
+    // Resolve react-native from app's node_modules for the wrapper (peerDependency)
+    extraNodeModules: {
+      'react-native': path.resolve(__dirname, 'node_modules/react-native'),
+    },
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
