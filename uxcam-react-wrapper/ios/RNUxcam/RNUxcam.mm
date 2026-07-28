911446a3b2de84a37657bc8289b5f143e7d61d05
Rajendra karki <rajendra@uxcam.com>
2026-07-28 18:05:04 +0545

    feat: update iOS SDK to 3.9.1 and android SDK to 3.10.7

--- uxcam-react-wrapper/ios/RNUxcam/RNUxcam.mm
+++ uxcam-react-wrapper/ios/RNUxcam/RNUxcam.mm
@@ -32,7 +32,7 @@ static NSString* const RNUxcam_HideGestures = @"hideGestures";
 static NSString* const RNUxcam_OverlayColor = @"color";
 
 static NSString* const RNUxcam_PluginType = @"react-native";
-static NSString* const RNUxcam_PluginVersion = @"6.0.19";
+static NSString* const RNUxcam_PluginVersion = @"6.0.20";
 
 
 @interface RNUxcam ()
